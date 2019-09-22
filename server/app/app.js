d6fe20e8993adcce80151bb6f828b93ee41efc9d
Christian Dominguez <christian@redbonzai.com>
2019-09-22 07:03:09 -0500

    Modified.auth.js to include requested scopes.  Storing requested scopes in localStorage, and removing them on logout().  Added method: userHasScopes() to be used on server side.

---
+++ server/app/app.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import jwt from 'express-jwt';
+import jwksRsa from 'jwks-rsa';
+
+const app = express();
+
+const checkJwt = jwt({
+    // Dynamically provide a signing key based on the kid in the header
+    // and the signing keys provided by the JWKS endpoint
+    secret: jwksRsa.expressJwtSecret({
+        cache: true, //cache the signing key
+        rateLimit: true,
+        jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
+        jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // validate the audience and the issuer
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+
+  // this must match the algorithm selected in the auth0 dashboard advanced settings in OAuth tab
+  algorithms: ['RS256']
+});
+
+//app.use(checkJwt);
+
+app.get('/authorized', function (req, res) {
+  res.send('Secured Resource');
+});
+
+app.get('/public', (req, res) => {
+    res.json({
+        message: 'Hello from a public API'
+    })
+})
+
+app.get('/private', checkJwt, (req, res) => {
+    res.json({
+        message: 'Hello from a Private API'
+    })
+})
+
+app.listen(process.env.REACT_APP_PORT || 3001)
+console.log(`API server listening on ${process.env.REACT_APP_PORT}`, `Audience: `,process.env.REACT_APP_AUTH0_AUDIENCE)
--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -5,6 +5,7 @@ export default class Auth {
     constructor(history) {
         this.history = history;
         this.userProfile = null;
+        this.requestedScopes = 'openid profile email read:courses'
 
         this.auth0 = new auth0.WebAuth({
             domain: process.env.REACT_APP_AUTH0_DOMAIN,
@@ -12,7 +13,7 @@ export default class Auth {
             redirectUri: process.env.REACT_APP_AUTH0_CALLBACK_URL,
             audience: process.env.REACT_APP_AUTH0_AUDIENCE,
             responseType: 'token id_token',
-            scope: 'openid profile'
+            scope: this.requestedScopes
 
         })
     }
@@ -37,9 +38,21 @@ export default class Auth {
         // set the time that the access token will expire
         const expiresAt = JSON.stringify(authResult.expiresIn * 1000 + new Date().getTime())
 
+        // If there is a value on the `scope` param of the authResult, 
+        // then use it to set scopes in the session for the user.  Otherwise, 
+        // use the scopes as requested.  If no scopes were requested, then set it to empty string.
+        const scopes = authResult.scope || this.requestedScopes || ''
+
         localStorage.setItem('access_token', authResult.accessToken)
         localStorage.setItem('id_token', authResult.idToken)
         localStorage.setItem('expires_at', expiresAt)
+
+        /** 
+         * we are storing expires_at & scopes for convenience.  
+         * That way we don't have to parse the JWT 
+         * on the client to use this data for UI-related logic
+         */
+        localStorage.setItem('scopes', JSON.stringify(scopes))
     }
 
     isUserAuthenticated() {
@@ -51,6 +64,7 @@ export default class Auth {
         localStorage.removeItem('access_token')
         localStorage.removeItem('id_token')
         localStorage.removeItem('expires_at')
+        localStorage.removeItem('scopes')
         this.userProfile = null
 
         this.auth0.logout({
@@ -80,6 +94,13 @@ export default class Auth {
 
             callback(profile, err)
         })
+    }
+
+    userHasScopes = (scopes) => {
+        const grantedScopes = (
+            JSON.parse(localStorage.getItem('scopes')) || ''
+        ).split(' ');
+        return scopes.every(scope => grantedScopes.includes(scope))
     }
     
 }
