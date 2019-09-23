b5a09045b4bcbec079883ba1de6dd030d6fa8421
Christian Dominguez <christian@redbonzai.com>
2019-09-23 05:44:04 -0500

    Created AuthContext in order to pass the auth props to child components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomeComponent from './components/HomeComponent';
 import ProfileComponent from './components/ProfileComponent';
 import NavComponent from './components/NavComponent';
 import Auth from './Auth/Auth';
+import AuthContext from './Auth/AuthContext';
 import CallBackComponent from './components/CallBackComponent';
 import PublicComponent from './components/PublicComponent';
 import PrivateComponent from './components/PrivateComponent';
@@ -14,36 +15,41 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    // App gets access to history object from React Router
-    this.auth = new Auth(this.props.history)
+    this.state = {
+      // App gets access to history object from React Router
+      auth: new Auth(this.props.history)
+    }
   }  
 
   render() {
+    const { auth } = this.state
     return (
-      <>
-        <NavComponent auth={this.auth}/>
-        <div className='body'>
-          <Route 
-            path='/' 
-            exact 
-            render={props => <HomeComponent auth={this.auth} {...props} />}
-          />
-          <Route 
-            path='/callback' 
-            exact 
-            render={props => <CallBackComponent auth={this.auth} {...props} />}
-          />
-          <Route path='/public' component={PublicComponent} />
-          <PrivateRoute path='/profile' component={ProfileComponent} auth={this.auth} />          
-          <PrivateRoute path='/private' component={PrivateComponent} auth={this.auth} />
-          <PrivateRoute 
-            path='/courses' 
-            component={CoursesComponent}
-            auth={this.auth}
-            scopes={['read:courses']} 
-          />
-        </div>
-      </>
+      <AuthContext.Provider value={auth}>
+        <>
+          <NavComponent auth={auth}/>
+          <div className='body'>
+            <Route 
+              path='/' 
+              exact 
+              render={props => <HomeComponent auth={auth} {...props} />}
+            />
+            <Route 
+              path='/callback' 
+              exact 
+              render={props => <CallBackComponent auth={auth} {...props} />}
+            />
+            <Route path='/public' component={PublicComponent} />
+            <PrivateRoute path='/profile' component={ProfileComponent} auth={auth} />          
+            <PrivateRoute path='/private' component={PrivateComponent} auth={auth} />
+            <PrivateRoute 
+              path='/courses' 
+              component={CoursesComponent}
+              auth={auth}
+              scopes={['read:courses']} 
+            />
+          </div>
+        </>
+      </AuthContext.Provider>
     );
   }
 }
